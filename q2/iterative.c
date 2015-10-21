ec08c68718f6c2b62e48c6c397723f289395ea0c
GreenYo0413 <f0246810@gmail.com>
2015-10-21 15:08:22 +0800

    move the file from old repository to a new one

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,4 +1,32 @@
-/* FIXME: Implement! */
+#include<stdio.h>
+#include<string.h>
+#define MAX_SIZE 1000
+char smallest_character(char str[], char c);
+
+int main(int argc, char *argv[]){
+	char array[MAX_SIZE];
+	if(argc==3){
+		strcpy(array,argv[1]);
+		printf("%c\n",smallest_character(array,argv[2][0]));
+	}
+	else
+		printf("error\n");
+
+	return 0;
+}
+
+char smallest_character(char str[], char c){
+	int count;
+	char smallestChar=str[0];
+	char goalChar='z'+1;
+	for(count=0;str[count]!=0;count++){
+		if(str[count]>=c && str[count]<=goalChar)
+			goalChar=str[count];
+		if(str[count]<smallestChar)
+			smallestChar=str[count];
+	}
+	return (goalChar=='z'+1)?smallestChar:goalChar;
+}/* FIXME: Implement! */
 
 int main()
 {
--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,32 @@
-/* FIXME: Implement! */
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-int main()
-{
-    return 0;
+int maxSubArray(int A[], int n);
+
+int main(int argc, char *argv[]){
+	
+	int array[9]={-2,1,-3,4,-1,2,1,-5,4};
+	printf("%d\n",maxSubArray(array,9));
+	return 0;
+}
+
+int maxSubArray(int A[], int n){
+	
+	if(n==0)
+		return -1;
+	int max=A[0];
+	int i,j;	
+	for(i=0;i<n;i++){
+		int localmax=A[i];
+		int current=0;
+		for(j=i;j<n;j++){
+			current+=A[j];
+			if(localmax<current)
+				localmax=current;
+		}
+		if(max<localmax)
+			max=localmax;
+	}
+	return max;
 }
